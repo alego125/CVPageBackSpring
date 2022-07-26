7c1313c5daa21adf489a035761c9dc611b157a0b
Ale Gomez <alejandro.gomez969@gmail.com>
2022-07-26 12:46:54 -0300

    Agregado localhost a cors

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# tutorial_jwt_BACK
+# BackEnd Proyecto Argentina Programa
 jwt security api REST 
-
 basic CRUD with JWT authentication based on roles
+
+Creacion de backend con springboot y spring security
--- src/main/java/com/tutorial/crud/CrudApplication.java
+++ src/main/java/com/tutorial/crud/CrudApplication.java
@@ -21,6 +21,7 @@ public class CrudApplication {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedOrigins(Arrays.asList("https://argprogram.web.app"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200/"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-Vith",
                 "Access-Control-Request-Nethod", "Access-Control-Request-Headers"));
